6fea6e6ef8a2548322196e596c8ea6c24901db2c
Shay-Ostrovsky <shay050604@gmail.com>
2026-05-07 21:42:56 +0300

    Convert icon.png to icon.ico; remove auto-conversion from shortcut script

--- create_shortcut.ps1
+++ create_shortcut.ps1
@@ -21,28 +21,13 @@ if (-not (Test-Path $pythonw)) {
     exit 1
 }
 
-# Convert icon.png -> icon.ico automatically if icon.png exists
-$iconPng = Join-Path $appDir "icon.png"
-$iconIco = Join-Path $appDir "icon.ico"
-if (Test-Path $iconPng) {
-    $python = $pythonw -replace "pythonw\.exe","python.exe"
-    & $python -c "
-from PIL import Image
-src = r'$iconPng'
-out = r'$iconIco'
-img = Image.open(src).convert('RGBA')
-sizes = [256,128,64,48,32,16]
-frames = [img.resize((s,s), Image.LANCZOS) for s in sizes]
-frames[0].save(out, format='ICO', sizes=[(s,s) for s in sizes], append_images=frames[1:])
-"
-}
-
 $shell                     = New-Object -ComObject WScript.Shell
 $shortcut                  = $shell.CreateShortcut($lnkPath)
 $shortcut.TargetPath       = $pythonw
 $shortcut.Arguments        = "`"$mainFile`""
 $shortcut.WorkingDirectory = $appDir
 $shortcut.Description      = "SelectAndRead"
+$iconIco = Join-Path $appDir "icon.ico"
 if (Test-Path $iconIco) {
     $shortcut.IconLocation = $iconIco
 } else {
