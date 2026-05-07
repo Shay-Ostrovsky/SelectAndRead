aaa5335abcbf31b842e2584628fdf6c35dc962f2
Shay-Ostrovsky <shay050604@gmail.com>
2026-05-07 21:41:58 +0300

    Auto-convert icon.png to icon.ico in create_shortcut.ps1

--- create_shortcut.ps1
+++ create_shortcut.ps1
@@ -21,15 +21,30 @@ if (-not (Test-Path $pythonw)) {
     exit 1
 }
 
+# Convert icon.png -> icon.ico automatically if icon.png exists
+$iconPng = Join-Path $appDir "icon.png"
+$iconIco = Join-Path $appDir "icon.ico"
+if (Test-Path $iconPng) {
+    $python = $pythonw -replace "pythonw\.exe","python.exe"
+    & $python -c "
+from PIL import Image
+src = r'$iconPng'
+out = r'$iconIco'
+img = Image.open(src).convert('RGBA')
+sizes = [256,128,64,48,32,16]
+frames = [img.resize((s,s), Image.LANCZOS) for s in sizes]
+frames[0].save(out, format='ICO', sizes=[(s,s) for s in sizes], append_images=frames[1:])
+"
+}
+
 $shell                     = New-Object -ComObject WScript.Shell
 $shortcut                  = $shell.CreateShortcut($lnkPath)
 $shortcut.TargetPath       = $pythonw
 $shortcut.Arguments        = "`"$mainFile`""
 $shortcut.WorkingDirectory = $appDir
 $shortcut.Description      = "SelectAndRead"
-$iconPath = Join-Path $appDir "icon.ico"
-if (Test-Path $iconPath) {
-    $shortcut.IconLocation = $iconPath
+if (Test-Path $iconIco) {
+    $shortcut.IconLocation = $iconIco
 } else {
     $shortcut.IconLocation = "$pythonw,0"
 }
